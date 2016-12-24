ed1f47008d3eb4e5e54276b639f7c23afeb56854
Diorgenes <dyogenes_gd@hotmail.com>
2016-12-24 16:39:17 -0300

    Added multi cust variants for miui ...and fix for (Added: device_features > https://github.com/DiorgenesS/MIUI_Nexus_4/commit/149465b892aaef1ef38e2919fb506b4bfd469fe5)

--- customize_target_files.sh
+++ customize_target_files.sh
@@ -9,5 +9,9 @@ OTHER_DIR=$PWD/other
 cp -f other/file_contexts out/target_files/META/
 rm -rf out/target_files/SYSTEM/vendor/preinstall
 
-+#Added device features
-+cp -f other/mako.xml out/target_files/SYSTEM/etc/device_features
+#Added multi cust variants for miui
+rm -rf out/target_files/DATA/miui/cust
+cp -rf other/cust out/target_files/DATA/miui
+
+#Added device features
+cp -f other/mako.xml out/target_files/SYSTEM/etc/device_features
